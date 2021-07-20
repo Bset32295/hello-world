e379c3fba571af3f1e7a9ce9809192d510a5463e
Bset32295 <87640648+Bset32295@users.noreply.github.com>
2021-07-20 10:50:32 +0700

    add last name and change function print

--- test_github.py
+++ test_github.py
@@ -1,2 +1,3 @@
-user_name = input("In put your name")
-print("Welcome '{}'".format(user_name))
+user_name = input("In put your first name")
+last_name = input("In put your last name")
+print("Welcome '{}'".format(user_name) + last_name)
